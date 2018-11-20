fda547b3f2346e7e632aeab66ed45516476f2e8c
LeonMuth <44811444+LeonMuth@users.noreply.github.com>
2018-11-20 16:58:21 +0100

    Update test.php
    DBConnect#2

--- test.php
+++ test.php
@@ -2,12 +2,25 @@
 //Fehlerbehandlung
 error_reporting (0);
 
-$db = new mysqli('localhost', 'root','','test');
+$db = new mysqli('localhost', 'root','','testdb');
 print_r($db->connect_error);
 
 if($db->connect_error){
     die('Verbindung nicht möglich!');
 }
-echo "Connected successfully";
+echo "Connected successfully <br>";
+
+$sql = "SELECT * FROM user";
+$result = $db->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "Age: " . $row["age"]. " - Name: " . $row["name"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
 
 ?>
